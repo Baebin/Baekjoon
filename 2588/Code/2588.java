fcfe14e11f79ebcf4c940526bd3a310d78aa9d9b
Baebin <63220297+Baebin@users.noreply.github.com>
2021-10-13 21:45:02 +0900

    Java Source
    2588

---
+++ 2588/Code/2588.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        int a, b;
+
+        Scanner scanner = new Scanner(System.in);
+        a = scanner.nextInt();
+        b = scanner.nextInt();
+        
+        System.out.println(a*(b%10));
+        System.out.println(a*((b/10)%10));
+        System.out.println(a*(b/100));
+        System.out.println(a*b);
+    }
+}
