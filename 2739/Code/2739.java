16b6c50a3558a497d7aae8a9ee05ea6ed4b65099
Baebin <63220297+Baebin@users.noreply.github.com>
2021-10-29 21:41:18 +0900

    Java Source
    2739

---
+++ 2739/Code/2739.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) {
+		int n;
+
+		Scanner scanner = new Scanner(System.in);
+		n = scanner.nextInt();
+		scanner.close();
+
+		for (int i = 1; i <= 9; i++) {
+			System.out.println(n + " * " + i + " = " + (n * i));
+		}
+	}
+}
