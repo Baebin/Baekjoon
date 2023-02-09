e3ff11697ad267c7281c7312e8a4c6226c1dd850
Baebin <63220297+Baebin@users.noreply.github.com>
2023-02-10 00:48:41 +0900

    [Bronze V] Title: 문자열, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/9086. 문자열/문자열.cc
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <string.h>
+
+int main() {
+	int T;
+	char word[1001];
+
+	scanf("%d", &T);
+	for (int i = 0; i < T; i++) {
+		scanf("%s", &word);
+		printf("%c%c\n", word[0], word[strlen(word) - 1]);
+	}
+}
