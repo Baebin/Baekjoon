f0481979c3f2525455e7374eb2c01a1735f9b3a9
Baebin <63220297+Baebin@users.noreply.github.com>
2023-02-10 00:46:42 +0900

    [Bronze I] Title: 최대공약수와 최소공배수, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2609. 최대공약수와 최소공배수/최대공약수와 최소공배수.cc
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+int gcd(int a, int b) {
+	int c;
+	while (b != 0) {
+		c = a % b;
+		a = b;
+		b = c;
+	}
+	return a;
+}
+
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+
+	int A, B;
+	cin >> A >> B;
+
+	int _gcd = gcd(A, B);
+	cout << _gcd << "\n";
+	cout << (A * B)/_gcd;
+}
