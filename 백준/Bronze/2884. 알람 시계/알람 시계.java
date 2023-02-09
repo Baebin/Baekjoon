a4966339a211939b701b2d9e7a7a6a6457a2f193
Baebin <63220297+Baebin@users.noreply.github.com>
2023-02-10 00:46:04 +0900

    [Bronze III] Title: 알람 시계, Time: 248 ms, Memory: 18392 KB -BaekjoonHub

---
+++ 백준/Bronze/2884. 알람 시계/알람 시계.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) {
+		int a, b;
+
+		Scanner scanner = new Scanner(System.in);
+		a = scanner.nextInt();
+		b = scanner.nextInt();
+		scanner.close();
+
+		b -= 45;
+		if (b < 0) {
+			a--;
+			b += 60;
+		}
+
+		if (a <0) {
+			a += 24;
+		}
+
+		System.out.print(a + " " + b);
+	}
+}
