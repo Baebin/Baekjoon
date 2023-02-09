60f9a7441c2e11fb0f351f077fe8da4f338d8e4a
Baebin <63220297+Baebin@users.noreply.github.com>
2023-02-09 23:55:29 +0900

    [Silver III] Title: 2×n 타일링 2, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/11727. 2×n 타일링 2/2×n 타일링 2.cc
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+#define MAX 1000
+#define MOD 10007
+
+vector<int> vec(MAX + 1);
+
+int dp(const int &N) {
+    /*
+    f(1) = 1
+    f(2) = 3
+    f(3) = 5
+    */
+
+    vec[1] = 1;
+    vec[2] = 3;
+
+    for (int i = 3; i <= N; i++)
+        vec[i] = (vec[i - 1] + vec[i - 2] * 2) % MOD;
+
+    return vec[N];
+}
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    int N;
+    cin >> N;
+
+    cout << dp(N);
+}
