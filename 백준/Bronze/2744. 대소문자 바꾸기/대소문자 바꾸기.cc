ef80b62e768c87522e273fbc3b62928bcbb1e048
Baebin <63220297+Baebin@users.noreply.github.com>
2023-02-10 00:49:39 +0900

    [Bronze V] Title: 대소문자 바꾸기, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/2744. 대소문자 바꾸기/대소문자 바꾸기.cc
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <string.h>
+
+int main() {
+	char str[100];
+	scanf("%s", &str);
+
+	for (int i = 0; i < strlen(str); i++) {
+		if (str[i] >= 97) {
+			str[i] -= 32;
+		} else {
+			str[i] += 32;
+		}
+	}
+	printf("%s", str);
+}
