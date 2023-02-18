17911ce83383e4c107faa0c37e5470df60c1c582
Baebin <63220297+Baebin@users.noreply.github.com>
2023-02-18 22:19:33 +0900

    [Bronze V] Title: 조별과제를 하려는데 조장이 사라졌다, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/15727. 조별과제를 하려는데 조장이 사라졌다/조별과제를 하려는데 조장이 사라졌다.cc
@@ -0,0 +1,16 @@
+#pragma GCC optimize("O3")
+#pragma GCC optimize("Ofast")
+#pragma GCC optimize("unroll-loops")
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    
+    int L;
+    cin >> L;
+    cout << ((L / 5) + !(L  % 5 == 0));
+}
