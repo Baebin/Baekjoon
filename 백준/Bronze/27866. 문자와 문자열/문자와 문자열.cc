fc50dc6e7ffcae34fe3803ba109d312841a91cb2
Baebin <63220297+Baebin@users.noreply.github.com>
2023-03-16 14:58:41 +0900

    [Bronze V] Title: 문자와 문자열, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/27866. 문자와 문자열/문자와 문자열.cc
@@ -0,0 +1,19 @@
+#pragma GCC optimize("O3")
+#pragma GCC optimize("Ofast")
+#pragma GCC optimize("unroll-loops")
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    string str;
+    cin >> str;
+    
+    int N;
+    cin >> N;
+    cout << str[N-1];
+}
