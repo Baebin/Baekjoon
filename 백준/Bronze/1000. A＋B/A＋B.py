52680fdc0d8e3c86826767c1ae8513c9e19743f8
Baebin <63220297+Baebin@users.noreply.github.com>
2023-04-23 21:27:24 +0900

    [Bronze V] Title: A+B, Time: 68 ms, Memory: 29200 KB -BaekjoonHub

--- 백준/Bronze/1000. A＋B/A＋B.py
+++ 백준/Bronze/1000. A＋B/A＋B.py
@@ -1,6 +1,12 @@
-a, b = input().split();
-
-a = int(a);
-b = int(b);
-
-print(a+b);
+<html>
+<head><title>502 Bad Gateway</title></head>
+<body>
+<center><h1>502 Bad Gateway</h1></center>
+</body>
+</html>
+<!-- a padding to disable MSIE and Chrome friendly error page -->
+<!-- a padding to disable MSIE and Chrome friendly error page -->
+<!-- a padding to disable MSIE and Chrome friendly error page -->
+<!-- a padding to disable MSIE and Chrome friendly error page -->
+<!-- a padding to disable MSIE and Chrome friendly error page -->
+<!-- a padding to disable MSIE and Chrome friendly error page -->
