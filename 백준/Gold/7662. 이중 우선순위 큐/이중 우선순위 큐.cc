2890b171b9ef9415bdb43fcf05c419a3d5705336
Baebin <63220297+Baebin@users.noreply.github.com>
2023-09-18 20:04:02 +0900

    [Gold IV] Title: 이중 우선순위 큐, Time: 1724 ms, Memory: 57124 KB -BaekjoonHub

--- 백준/Gold/7662. 이중 우선순위 큐/이중 우선순위 큐.cc
+++ 백준/Gold/7662. 이중 우선순위 큐/이중 우선순위 큐.cc
@@ -7,31 +7,54 @@
 #include <map>
 using namespace std;
 
-void pop(bool isMin, priority_queue<int>* max_que, priority_queue<int, vector<int>, greater<int>>* min_que, map<int, int>* cnt) {
-    int num;
+class dual_priority_queue {
+public:
+    void push(int num) {
+        cnt[num]++;
 
-    if (isMin) {
-        if ((*min_que).empty()) return;
-        num = (*min_que).top();
+        min_que.push(num);
+        max_que.push(num);
     }
-    else {
-        if ((*max_que).empty()) return;
-        num = (*max_que).top();
+
+    void pop(bool isMin) {
+        int num;
+
+        if (isMin) {
+            if (min_que.empty()) return;
+            num = min_que.top();
+        }
+        else {
+            if (max_que.empty()) return;
+            num = max_que.top();
+        }
+        cnt[num]--;
+
+        // Empty Number Delete
+        while (!min_que.empty() && cnt[min_que.top()] == 0)
+            min_que.pop();
+        while (!max_que.empty() && cnt[max_que.top()] == 0)
+            max_que.pop();
+
+        // Number Delete
+        if (!min_que.empty() && min_que.top() == num)
+            min_que.pop();
+        if (!max_que.empty() && max_que.top() == num)
+            max_que.pop();
     }
-    (*cnt)[num]--;
 
-    // Empty Number Delete
-    while (!(*min_que).empty() && (*cnt)[(*min_que).top()] == 0)
-        min_que->pop();
-    while (!(*max_que).empty() && (*cnt)[(*max_que).top()] == 0)
-        max_que->pop();
+    bool empty() {
+        return max_que.empty() || min_que.empty();
+    }
 
-    // Number Delete
-    if (!(*min_que).empty() && (*min_que).top() == num)
-        min_que->pop();
-    if (!(*max_que).empty() && (*max_que).top() == num)
-        max_que->pop();
-}
+    pair<int, int> front() {
+        return { max_que.top(), min_que.top() };
+    }
+
+private:
+    map<int, int> cnt;
+    priority_queue<int> max_que;
+    priority_queue<int, vector<int>, greater<int>> min_que;
+};
 
 int main() {
     ios::sync_with_stdio(false);
@@ -45,24 +68,23 @@ int main() {
         int K;
         cin >> K;
 
-        map<int, int> cnt;
-        priority_queue<int> max_que;
-        priority_queue<int, vector<int>, greater<int>> min_que;
+        dual_priority_queue que;
 
         while (K--) {
             char cmd;
             int num;
 
             cin >> cmd >> num;
-            if (cmd == 'I') {
-                cnt[num]++;
-                max_que.push(num);
-                min_que.push(num);
-            }
-            else pop(num == -1, &max_que, &min_que, &cnt);
+            if (cmd == 'I') que.push(num);
+            else que.pop(num == -1);
         }
 
-        if (max_que.empty() || min_que.empty()) cout << "EMPTY" << "\n";
-        else cout << max_que.top() << " " << min_que.top() << "\n";
+        if (que.empty()) {
+            cout << "EMPTY" << "\n";
+            continue;
+        }
+
+        pair<int, int> data = que.front();
+        cout << data.first << " " << data.second << "\n";
     }
 }
