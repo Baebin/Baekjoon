cfc914e8f0dff280d49db0edead0b66a4b035c14
Baebin <63220297+Baebin@users.noreply.github.com>
2023-10-25 10:58:01 +0900

    [Diamond V] Title: gcd(n, k) = 1, Time: 24 ms, Memory: 2032 KB -BaekjoonHub

---
+++ 백준/Diamond/13926. gcd（n， k） ＝ 1/gcd（n， k） ＝ 1.cc
@@ -0,0 +1,136 @@
+#pragma GCC optimize("O3")
+#pragma GCC optimize("Ofast")
+#pragma GCC optimize("unroll-loops")
+
+#include <iostream>
+#include <vector>
+#include <cstdlib>
+#include <algorithm>
+using namespace std;
+
+#define abs(X) ((X) > (0) ? (X) : -(X))
+#define mul(X, Y, M) (((ui128)X * Y) % M)
+
+// Structure
+typedef unsigned long long ull;
+typedef unsigned __int128 ui128;
+
+class MillerRabin {
+public:
+    // Check N
+    bool isPrime(const ull& N) {
+        for (const ull& prime : primes)
+            if (!checkPrime(N, prime))
+                return false;
+        return true;
+    }
+protected:
+    // X ^ Y % M
+    ull fastPower(ull X, ull Y, const ull& M) {
+        ull power = 1;
+        while (Y) {
+            if (Y % 2)
+                power = mul(X, power, M);
+            X = mul(X, X, M);
+            Y >>= 1;
+        }
+        return power;
+    }
+    bool checkPrime(const ull& N, const ull& A) {
+        // Prime Check
+        if (A % N == 0)
+            return true;
+        ull K = N - 1;
+        while (true) {
+            ull power = fastPower(A, K, N);
+            // A ^ K % N == 1 || -1
+            if (K % 2)
+                return (power == 1 || power == N - 1);
+            // A ^ K % N == -1
+            if (power == N - 1)
+                break;
+            K >>= 1;
+        }
+        return true;
+    }
+private:
+    const vector<ull> primes = { 2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37 };
+};
+
+class PollardRho {
+public:
+    explicit PollardRho(const ull& N) : N(N) { init(); }
+    vector<ull> getPrimes() { return primes; }
+protected:
+    void init() {
+        while (N != 1) {
+            ull prime = find(N);
+            primes.emplace_back(prime);
+            N /= prime;
+        }
+    }
+    ull find(const ull& N) {
+        if (N % 2 == 0)
+            return 2;
+        if (millerRabin.isPrime(N))
+            return N;
+        ull X, Y, C, G = N;
+        auto f = [&](const ull& A) { return (mul(A, A, N) + C) % N; };
+        do {
+            if (G == N) {
+                X = Y = rand() % (N - 2) + 2;
+                C = rand() % 10 + 1;
+            }
+            X = f(X), Y = f(f(Y));
+            G = gcd(abs(X - Y), N);
+        } while (G == 1);
+        if (millerRabin.isPrime(G))
+            return G;
+        return find(G);
+    }
+    ull gcd(ull A, ull B) {
+        if (A < B)
+            swap(A, B);
+        while (B) {
+            ull R = A % B;
+            A = B;
+            B = R;
+        }
+        return A;
+    }
+private:
+    ull N;
+    vector<ull> primes;
+    MillerRabin millerRabin;
+};
+
+class EulerPhi {
+public:
+    ull get(const ull& N) {
+        vector<ull> primes = PollardRho(N).getPrimes();
+        sort(primes.begin(), primes.end());
+        primes.emplace_back(0);
+
+        ull cnt = 1, k = 1;
+        const ull& len = primes.size();
+        for (register ull i = 1; i < len; i++) {
+            if (primes[i - 1] == primes[i]) {
+                k++;
+                continue;
+            }
+            const ull& p = primes[i - 1];
+            ull pk = 1;
+            while (k--)
+                pk *= p;
+            k = 1;
+            cnt *= (pk - pk/p);
+        }
+        return cnt;
+    }
+};
+
+int main() {
+    ull N;
+    cin >> N;
+    cout << EulerPhi().get(N);
+}
