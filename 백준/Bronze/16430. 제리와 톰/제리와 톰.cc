9a586267f9df2ab5521aa32b75d02b1a4c6c5143
Baebin <63220297+Baebin@users.noreply.github.com>
2025-04-03 15:35:02 +0900

    [Bronze V] Title: 제리와 톰, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/16430. 제리와 톰/제리와 톰.cc
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <vector>
+#include <queue>
+using namespace std;
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cout.tie(nullptr);
+    cin.tie(nullptr);
+
+    int A, B;
+    cin >> A >> B;
+
+    cout << B - A << ' ' << B;
+}
+
